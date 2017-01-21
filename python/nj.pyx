a917a64a7754eb4fb9f4c66f53683890f62669be
Simon Frost <sdwfrost@gmail.com>
2017-01-21 06:30:46 +0000

    Added NJ* algorithm

--- python/nj.pyx
+++ python/nj.pyx
@@ -1,7 +1,8 @@
 from libc.stdlib cimport malloc, free
 
 cdef extern from "../src/nj.h":
-  double C_nj (double *D, int *N, int *edge1, int *edge2, double *edge_length)
+  int C_nj (double *D, int *N, int *edge1, int *edge2, double *edge_length)
+  int C_njs(double *D, int *N, int *edge1, int *edge2, double *edge_length, int *fsS);
 
 def nj(list d, int n):
   cdef double *D =  <double*>malloc(len(d)*sizeof(double))
@@ -14,12 +15,29 @@ def nj(list d, int n):
   cdef int *edge1 = <int*>malloc(M*sizeof(int))
   cdef int *edge2 = <int*>malloc(M*sizeof(int))
   cdef double *edge_length = <double*>malloc(M*sizeof(double))
-  C_nj(D, &N, edge1, edge2, edge_length)
+  cdef int err = C_nj(D, &N, edge1, edge2, edge_length)
   cdef list result = [[edge1[i],edge2[i],edge_length[i]] for i in xrange(M)]
   free(edge1)
   free(edge2)
   free(edge_length)
   return(result)
 
+def njs(list d, int n, int fs):
+  cdef double *D =  <double*>malloc(len(d)*sizeof(double))
+  if D is NULL:
+    raise MemoryError()
+  for i in xrange(len(d)):
+    D[i]=d[i]
+  cdef int N = n
+  cdef int M = 2*N-3
+  cdef int *edge1 = <int*>malloc(M*sizeof(int))
+  cdef int *edge2 = <int*>malloc(M*sizeof(int))
+  cdef double *edge_length = <double*>malloc(M*sizeof(double))
+  cdef int err = C_njs(D, &N, edge1, edge2, edge_length, &fs)
+  cdef list result = [[edge1[i],edge2[i],edge_length[i]] for i in xrange(M)]
+  free(edge1)
+  free(edge2)
+  free(edge_length)
+  return(result)
 
 
--- src/nj.h
+++ src/nj.h
@@ -3,6 +3,13 @@
 
 #define DINDEX(i, j) n*(i - 1) - i*(i - 1)/2 + j - i - 1
 
-extern void C_nj(double *D, int *N, int *edge1, int *edge2, double *edge_length);
+int choosePair(double* D, int n, double* R, int* s, int* sw, int* x, int* y, int fS);
+double cnxy(int x, int y, int n, double* D);
+int mxy(int x,int y, int n, double* D);
+double nxy(int x, int y, int n, double* D);
+int cxy(int x, int y, int n, double* D);
+
+extern int C_nj(double *D, int *N, int *edge1, int *edge2, double *edge_length);
+extern int C_njs(double *D, int *N, int *edge1, int *edge2, double *edge_length, int *fsS);
 
 #endif
